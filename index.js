d37a75eaf6af4aa550bcdfdec371f3bed68ac45c
Gergely Dancs <dancs.gergely@flexoon.hu>
2022-10-18 16:39:40 +0200

    add form

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const form = document.querySelector("form");
-const askofferBtn = document.getElementById("ask-offer");
-const closeFormBtn = document.querySelector("form > div");
 
-askofferBtn.addEventListener("click", () => form.classList.remove("hidden"));
-
-closeFormBtn.addEventListener("click", () => form.classList.add("hidden"));
