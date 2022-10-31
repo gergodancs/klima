c03411e528eced5d5b7ee47c3d3c78c3da50f0f2
Gergely Dancs <dancs.gergely@flexoon.hu>
2022-10-31 11:46:08 +0100

    remove netlify attribute

--- js/index.js
+++ js/index.js
@@ -98,7 +98,6 @@ const createDetailsPage = (item, listElement) => {
 const createOrderForm = (detailsContainer, item) => {
   const phoneContainer = document.createElement("form");
   phoneContainer.className = "phone-container";
-  phoneContainer.setAttribute("netlify");
 
   const phoneInput = document.createElement("input");
   phoneInput.setAttribute("placeholder", "Kérem adja meg telefonszámát:");
